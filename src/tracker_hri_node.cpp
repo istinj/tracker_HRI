660eb7b9cb150310e5c4df1a6bdb55b082fb40c1
istinj <istinj@gmail.com>
2016-11-24 18:52:56 +0100

    BG Sub

--- src/tracker_hri_node.cpp
+++ src/tracker_hri_node.cpp
@@ -19,22 +19,31 @@
 using namespace std;
 using namespace cv;
 
-// Declarations
+// --------------------------------------------------------------- //
+// ----------------------- Declarations -------------------------- //
+// --------------------------------------------------------------- //
 cv::Mat curr_frame_rgb, curr_frame_gray;
 
-// Face - eye detectors
-cv::CascadeClassifier face_cascade, eye_cascade;
-std::vector<cv::Rect> faces_vector, eyes_vector;
-string path_to_xml_face = "/home/istin/Documenti/1_CATKIN_SRCS/HRI_srcs/tracker_hri/misc/haarcascades/haarcascade_frontalface_alt.xml";
-string path_to_xml_eye = "/home/istin/Documenti/1_CATKIN_SRCS/HRI_srcs/tracker_hri/misc/haarcascades/haarcascade_eye_tree_eyeglasses.xml";
-// string path_to_xml_eye = "/home/istin/Documenti/1_CATKIN_SRCS/HRI_srcs/tracker_hri/misc/haarcascades/haarcascade_eye.xml";
-
-// Color generator
+// Random color generator
 cv::RNG rng(12345);
+
+// Face detector
+cv::CascadeClassifier face_cascade;
+std::vector<cv::Rect> faces_vector;
+string path_to_xml_face = "/home/istin/Documenti/1_CATKIN_SRCS/HRI_srcs/tracker_hri/misc/haarcascades/haarcascade_frontalface_alt.xml";
+
+// Background subtractor
+cv::Ptr<cv::BackgroundSubtractor> pMog2_sub;
+std::vector<cv::Point> contours;
+std::vector<cv::Vec4i> hierarchy;
+cv::Mat foreg_mask;
 
 
 void rgbTrackerCB(const sensor_msgs::ImageConstPtr& msg);
 
+// --------------------------------------------------------------- //
+// ------------------------- Functions --------------------------- //
+// --------------------------------------------------------------- //
 int main(int argc, char *argv[])
 {
 	ros::init(argc, argv, "tracker");
@@ -44,6 +53,7 @@ int main(int argc, char *argv[])
 	// ros::Subscriber rgb_sub = n.subscribe("/camera/rgb/image_color", 1, rgbTrackerCB);
 
 	// WebCam
+	pMog2_sub = new cv::BackgroundSubtractorMOG();
 	ros::Subscriber rgb_sub = n.subscribe("/image_raw", 1, rgbTrackerCB);
 
 	ros::spin();
@@ -74,43 +84,37 @@ void rgbTrackerCB(const sensor_msgs::ImageConstPtr& msg)
 		return;
 	}
 
-	out = eye_cascade.load(path_to_xml_face);
-	if (!out)
-	{
-		cerr << "ERROR Haar eye filter!" << endl;
-		return;
-	}
+	// Background subtractor
+	pMog2_sub->operator()(curr_frame_rgb, foreg_mask);
 
+	cv::namedWindow("Foreground mask");
+	cv::imshow("Foreground mask", foreg_mask);
+	cv::waitKey(15);
+
+
+
+
+
+
+	// Face detector
 	cv::cvtColor(curr_frame_rgb, curr_frame_gray, COLOR_BGR2GRAY);
 	cv::equalizeHist(curr_frame_gray, curr_frame_gray);
 
 
 	face_cascade.detectMultiScale(curr_frame_gray, faces_vector, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
 
-	//! For each face detected
 	for (int i = 0; i < faces_vector.size(); i++)
 	{
-		//! center of the ellipse
 		cv::Point face_center(faces_vector[i].x + faces_vector[i].width*0.5, 
 			faces_vector[i].y + faces_vector[i].height*0.5);
-		//! Drawing ellipses for each face found, on top of the RGB image
-		ellipse(curr_frame_rgb, face_center, Size( faces_vector[i].width*0.5, faces_vector[i].height*0.5), 
+
+		cv::ellipse(curr_frame_rgb, face_center, Size( faces_vector[i].width*0.5, faces_vector[i].height*0.5), 
 			0, 0, 360, Scalar(rng.uniform(0,255), rng.uniform(0,255), rng.uniform(0,255)), 4, 8, 0 );
 
 		cv::Mat face_mask = curr_frame_gray(faces_vector[i]);
-		eye_cascade.detectMultiScale(face_mask, eyes_vector);
-
-		//! For each eye
-		cout << eyes_vector.size() << endl;	
-		for (int j = 0; j < eyes_vector.size(); j++)
-		{
-			cv::Point eye_center(faces_vector[i].x + eyes_vector[j].x + eyes_vector[j].width*0.5, 
-				faces_vector[i].y + eyes_vector[j].y + eyes_vector[j].height*0.5);
-			int rad = cvRound((eyes_vector[j].width + eyes_vector[j].height) * 0.25f);
-			circle(curr_frame_rgb, eye_center, rad, Scalar(rng.uniform(0,255), 
-				rng.uniform(0,255), rng.uniform(0,255)), 4, 8, 0 );
-		}
 	}
+
+
 
 	cv::namedWindow("RGB Datastream", CV_WINDOW_NORMAL);
 	cv::imshow("RGB Datastream", curr_frame_rgb);
