ad981ba9cf8b24a5fc32a22f80b51d94eb9027af
istinj <istinj@gmail.com>
2016-12-11 15:49:22 +0100

    TODO: Exploit Laserscan Ranges In order to find out the depth values on whitch we must evaluate the person

--- src/tracker_hri_node.cpp
+++ src/tracker_hri_node.cpp
@@ -10,16 +10,20 @@
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/image_encodings.h>
+#include <laser_analysis/LaserObstacle.h>
+#include <laser_analysis/LaserObstacleMap.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
+#include <tf/transform_listener.h>
 //OpenCV
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/objdetect/objdetect.hpp>
-
-//BG SUBTRACTION
 #include <opencv2/video/background_segm.hpp>
+//EIGEN
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/Geometry>
 
 #define FPS (int)1000/30
 using namespace std;
@@ -29,8 +33,11 @@ using namespace cv;
 // ----------------------- Declarations -------------------------- //
 // --------------------------------------------------------------- //
 cv::Mat curr_frame_rgb, curr_frame_gray;
-std::vector<bool> flag_vec(2);
+bool show_images = false;
+bool obstacle = false;
 
+// Robot pose
+tf::TransformListener *listener=NULL;
 // Random color generator
 cv::RNG rng(12345);
 
@@ -44,11 +51,14 @@ std::vector<float> b_detector;
 cv::HOGDescriptor hog_descriptor;
 
 // Callbacks
-void rgbTrackerCB(const sensor_msgs::ImageConstPtr& msg);
-void depthTrackerCB(const sensor_msgs::ImageConstPtr& msg);
-void cmdVelTrackerCB(const geometry_msgs::TwistConstPtr& msg);
+void rgbCB(const sensor_msgs::ImageConstPtr& msg);
+void depthCB(const sensor_msgs::ImageConstPtr& msg);
+void laserScanCB(const sensor_msgs::LaserScanConstPtr& msg);
+void laserObsCB(const laser_analysis::LaserObstacleConstPtr& msg);
+void laserMapCB(const laser_analysis::LaserObstacleMapConstPtr& msg);
 // Utilities
 void display_image(const cv::Mat& image_, const std::string name_);
+void getRobotPose(Eigen::Vector3f& diago_pose_);
 
 // --------------------------------------------------------------- //
 // ------------------------- Functions --------------------------- //
@@ -69,6 +79,8 @@ int main(int argc, char *argv[])
 	b_detector = cv::HOGDescriptor::getDefaultPeopleDetector();
 	hog_descriptor.setSVMDetector(b_detector);
 
+	listener = new tf::TransformListener();
+
 	/* BAG TOPICS
 	 * /diago/cmd_vel -> geometry_msgs/Twist
 	 * /diago/odom -> nav_msgs/Odometry
@@ -84,16 +96,23 @@ int main(int argc, char *argv[])
 	std::string topic_rgb(argv[1]);
 	std::string topic_depth(argv[2]);
 	std::string topic_vel = "/diago/cmd_vel";
+	std::string topic_laser_scan = "/diago/scan";
+	std::string topic_laser_obs = "/diago/laser_obstacle";
+	std::string topic_laser_map = "/diago/laser_obstacle_map";
 	
-	ros::Subscriber rgb_sub = n.subscribe(topic_rgb, 1, rgbTrackerCB);
-	ros::Subscriber depth_sub = n.subscribe(topic_depth, 1, depthTrackerCB);
-	ros::Subscriber cmd_vel_sub = n.subscribe(topic_vel, 1, cmdVelTrackerCB);
+//	ros::Subscriber rgb_sub = n.subscribe(topic_rgb, 1, rgbCB);
+//	ros::Subscriber depth_sub = n.subscribe(topic_depth, 1, depthCB);
+	ros::Subscriber laser_scan_sub = n.subscribe(topic_laser_scan, 1, laserScanCB);
+	ros::Subscriber laser_obs_sub = n.subscribe(topic_laser_obs, 1, laserObsCB);
+	ros::Subscriber laser_map_sub = n.subscribe(topic_laser_map, 1, laserMapCB);
 
 	ros::spin();
 	return 0;
 }
 
-void rgbTrackerCB(const sensor_msgs::ImageConstPtr& msg)
+
+// CALLBACKS
+void rgbCB(const sensor_msgs::ImageConstPtr& msg)
 {
 	// Convert from sensor_msg to cv mat
 	cv_bridge::CvImageConstPtr cv_ptr;
@@ -113,16 +132,17 @@ void rgbTrackerCB(const sensor_msgs::ImageConstPtr& msg)
 //		cv::rectangle(curr_frame_gray, body_vector[i].tl(),
 //			body_vector[i].br(), cv::Scalar(180,10,10));
 //	}
-
-	display_image(curr_frame_gray, "HOGDescriptor");
-	display_image(curr_frame_rgb, "RGB Datastream");
-
+	if(show_images)
+	{
+		display_image(curr_frame_gray, "HOGDescriptor");
+		display_image(curr_frame_rgb, "RGB Datastream");
+	}
 	// cerr << "Publishing condition" << endl;
 	// system("rostopic pub /diago/PNPConditionEvent std_msgs/String \"data: \'pDetected\'\" --once");
 }
 
 
-void depthTrackerCB(const sensor_msgs::ImageConstPtr& msg) 
+void depthCB(const sensor_msgs::ImageConstPtr& msg) 
 {
 	// TOPIC /diago/top_camera/depth/image_raw
 	float min_range = 0.0f;
@@ -158,19 +178,57 @@ void depthTrackerCB(const sensor_msgs::ImageConstPtr& msg)
 			Ii[j] = (char) (255 * ((Di[j] - min_range)/(max_range - min_range)));
 		}
 	}
-
-	display_image(depth_image, "Depth Image");
+	if(show_images)
+		display_image(depth_image, "Depth Image");
 }
 
-void cmdVelTrackerCB(const geometry_msgs::TwistConstPtr& msg)
+
+void laserScanCB(const sensor_msgs::LaserScanConstPtr& msg)
 {
-	cout << CYAN << "cmd_vel angular\n" << msg->angular << RESET;
-	cout << RED << "cmd_vel linear\n" << msg->linear << RESET <<  endl;
+	if(!obstacle)
+		return;
+	cout << msg->ranges.size() << endl;
 }
 
+void laserObsCB(const laser_analysis::LaserObstacleConstPtr& msg)
+{
+	cout << msg->y1 << "\t" << msg->y2 << endl;
+}
+
+void laserMapCB(const laser_analysis::LaserObstacleMapConstPtr& msg)
+{
+	Eigen::Vector2f laser_map(msg->mx, msg->my);
+	if(laser_map.norm() > 0)
+	{
+		cout << YELLOW << "Obstacle detected!" << endl;
+		cout << "Analysing" << RESET  << endl;
+		obstacle = true;
+	}
+
+}
+
+
+// OTHER FUNCTIONS
 void display_image(const cv::Mat& image_, const std::string name_)
 {
 	cv::namedWindow(name_, CV_WINDOW_NORMAL);
 	cv::imshow(name_, image_);
 	cv::waitKey(FPS);
+}
+
+
+void getRobotPose(Eigen::Vector3f& diago_pose_)
+{
+	double roll, pitch, yaw;
+	tf::StampedTransform T;
+
+	listener->lookupTransform("/map","/diago/laser_frame", ros::Time(0), T);
+
+	tf::Quaternion q = T.getRotation();
+	tf::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+	diago_pose_ << (float)T.getOrigin().x() ,
+			(float)T.getOrigin().y(),
+			(float)yaw;
+	return;
 }
